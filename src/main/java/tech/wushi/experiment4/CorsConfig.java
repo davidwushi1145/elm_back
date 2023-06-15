cba40b8f96f04fa56c1ea283b56e504f9df28ff6
davidlee <david.wushi@foxmail.com>
2023-06-15 14:34:42 +0800

    bugs fix

--- src/main/java/tech/wushi/experiment4/CorsConfig.java
+++ src/main/java/tech/wushi/experiment4/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    // 当前跨域请求最大有效时长。这里默认1天
+    // 当前跨域请求最大有效时长。1天
     private static final long MAX_AGE = 24 * 60 * 60;
 
     @Bean
